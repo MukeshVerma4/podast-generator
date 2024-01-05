989c519c9906824e0a10e1476696f177ea57d1bd
MukeshVerma4 <150228531+MukeshVerma4@users.noreply.github.com>
2024-01-05 14:13:39 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ echo "=========="
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.dirctory /github/workspace
+git config --global safe.directory *
 python3 /usr/bin/feed.py
 git add -A && commit -m "Update feed"
 git push --set-upstream origin main
